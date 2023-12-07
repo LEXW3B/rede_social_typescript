932045b99964c509cd4931bc785ba74bee5f65e3
Alexandre E.S.L <alexmanson12345@gmail.com>
2023-12-07 08:03:45 -0300

    feat: iniciando projeto react com typescript e configurando rotas

---
+++ client/src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Esta é a página inicial do site.</p>
+    </div>
+  );
+};
---
+++ client/src/pages/Private/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Private = () => {
+  return (
+    <div>
+      <h1>Página Privada</h1>
+      <p>Esta é uma página privada.</p>
+    </div>
+  );
+};
